2a1eda73f8c3eee4194c6e81d7ebc5bf5065d1ed
=Norbert POINTU <=npointu@dxc.com>
2021-05-10 14:42:57 +0200

    Add configureStore

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react'
-
+import 'init'
 import App from './App'
 import React from 'react'
 
 test('renders learn react link', () => {
     render(<App />)
-    const linkElement = screen.getByText(/learn react/i)
+    const linkElement = screen.getByTestId('main_app')
     expect(linkElement).toBeInTheDocument()
 })
