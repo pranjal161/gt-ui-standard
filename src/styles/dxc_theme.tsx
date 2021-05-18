6dc87034ba7f1e527a9a1958a288430d7724eb1d
spamecha2 <suhani.pamecha@dxc.com>
2021-05-18 11:38:59 +0530

    DAPDIG-285

---
+++ src/styles/dxc_theme.tsx
@@ -0,0 +1,32 @@
+export const Themes = {
+    dxc_theme: {
+        foreground: '#ffffff',
+        background: '#222222',
+        headers: {
+            backgroundColor: 'black',
+        }
+    },
+};
+
+export const Colors = {
+    header: {
+        background: 'black',
+        color: 'white'
+    },
+    slider: {
+        color: '#000000'
+    },
+    table: {
+        headerBackgroundColor: '#FFFFFF',
+        headerFontColor: '#000000'
+    },
+    spinner: {
+        trackCircleColor: '#6F2C91',
+        totalCircleColor: '#FFFFFF'
+    },
+    tabs: {
+        selectedBackgroundColor: '#FFFFFF',
+        selectedUnderlinedColor: '#6F2C91',
+        selectedColor: '#FFFFFF'
+    }
+};
