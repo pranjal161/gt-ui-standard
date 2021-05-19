bf5ba3cefabffb1ae1692a681876c4f14a0f2991
akushwaha21 <akruti.kushwaha@dxc.com>
2021-05-19 18:22:26 +0530

    Hover button CSS

--- src/components/IconButton/StyledIconButton.ts
+++ src/components/IconButton/StyledIconButton.ts
@@ -3,5 +3,13 @@ import styled from 'styled-components';
 export const StyledIconButton = styled.span`
     button {
         padding: 12px 10px !important;
+
+        &:hover {
+            background: transparent !important;
+
+            svg {
+                fill: #000 !important;
+            }
+        }
     }
 `;
