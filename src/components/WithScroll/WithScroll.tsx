231b6dd7eba890208a7415c9ea69501dcbe478e0
=Norbert POINTU <=npointu@dxc.com>
2021-06-11 09:21:04 +0200

    Update WithScroll.tsx

--- src/components/WithScroll/WithScroll.tsx
+++ src/components/WithScroll/WithScroll.tsx
@@ -1,23 +1,51 @@
-import './WithScroll.css';
-
-import PropTypes from 'prop-types';
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 
-const WithScroll = (props: { visibleHeight: string, children: any }) => {
-    const { visibleHeight, children } = props;
 
-    return (
-        <div style={{ height: visibleHeight, overflowY: 'hidden' }}>
-            <div className="hide-scroll-bar" style={{ height: '100%', overflowY: 'scroll' }}>{children}</div>
-        </div>
-    )
+const useStyles = makeStyles((theme) => ({
+    '@global': {
+        '*::-webkit-scrollbar': {
+            width: '2px',
+        },
+        '*::-webkit-scrollbar-thumb': {
+            background: '#97a0af',
+            marginBlock: '8px',
+            borderRadius: '2px'
+        },
+        '*::-webkit-scrollbar-track': {
+            background: theme.palette.project.sidebar.toolbar.border,
+            borderRadius: '2px'
+        }
+    },
+    root: {
+        placeSelf: 'stretch',
+        height: '100%',
+        overflowY: 'scroll',
+    }
+}
+));
+
+export interface WithScrollProps {
+
+    /**
+     * Children
+     */
+    children: any
 }
 
-WithScroll.propTypes = {
-    visibleHeight: PropTypes.string,
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node])
+/**
+ * Apply scroll into the children
+ * @param {WithScrollProps} children props
+ * @constructor
+ */
+const WithScroll: React.FC<WithScrollProps> = ({children}: WithScrollProps) => {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root}>
+            {children}
+        </div>
+    )
 }
 
 export default WithScroll;
