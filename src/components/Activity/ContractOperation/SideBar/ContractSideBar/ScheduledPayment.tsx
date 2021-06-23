7783c7de6076b7b8bbb9f341873e796a8230539d
pshinde5 <pshinde5@dxc.com>
2021-06-23 11:50:54 +0530

    Contract Sidebar: Scheduled payment

---
+++ src/components/Activity/ContractOperation/SideBar/ContractSideBar/ScheduledPayment.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+
+import ContentList from '../components/ContentList';
+import { PanelSectionItem } from 'components/PanelSection/PanelSection';
+import { getLink } from 'utils/functions';
+import useAia from 'hooks/useAia';
+
+//import { useTranslation } from 'react-i18next';
+
+const ScheduledPayment = (props: { contractResponse: any }) => {
+    const contractResponse = props.contractResponse.data;
+    const [scheduledPayment, setScheduledPayment] = React.useState<undefined | any>();
+    const { fetch } = useAia();
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = () => {
+        if (contractResponse) {
+            const scheduledPaymentListUrl = getLink(contractResponse, 'contract:billing_list-scheduled_payment');
+            fetch(scheduledPaymentListUrl).then((itemsList: any) => {
+                if (itemsList && itemsList.data['_links'] && itemsList.data['_links'].item) {
+                    fetch(itemsList.data['_links'].item.href).then((response: any) => {
+                        setScheduledPayment(response.data)
+                    });
+                }
+            });
+        }
+    };
+    const Items: PanelSectionItem[] = [
+        { id: 'billing:amount', styleType: ['currency'] },
+        { id: 'billing:payment_type', styleType: ['text'] },
+        { id: 'billing:frequency', styleType: ['text'] },
+        { id: 'billing:next_due_date', styleType: ['date'] }
+    ];
+
+    return (
+        <>
+            {scheduledPayment && (
+                <>
+                    <ContentList items={Items} data={scheduledPayment} />
+                </>
+            )}
+        </>
+    );
+};
+
+export default ScheduledPayment;
