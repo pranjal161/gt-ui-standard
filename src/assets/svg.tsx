d6cfed910077c32f7c5cb5618ab3c9bd43f4654b
pshinde5 <pshinde5@dxc.com>
2021-06-30 20:34:18 +0530

    Generic Accordion

--- src/assets/svg.tsx
+++ src/assets/svg.tsx
@@ -103,3 +103,6 @@ export const ContactsIcon = () => (
     </svg>    
 );
 export const SquaredAddIcon = makeIcon('M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10h-4v4h-2v-4H7v-2h4V7h2v4h4v2z');
+export const ExpandMore = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/></svg>
+)
