4acd3865cb8f0f726acad5e534898bf58139c927
=Norbert POINTU <=npointu@dxc.com>
2021-07-05 16:50:05 +0200

    Add validate

--- src/components/StatusReportPreview/StatusReportPreview.tsx
+++ src/components/StatusReportPreview/StatusReportPreview.tsx
@@ -6,7 +6,7 @@ import {getStatusReport} from 'utils/functions';
 import useResponse from 'hooks/useResponse';
 
 const StatusReportPreview = ({hRef}: any) => {
-    const {setFocusError} = useStep()
+    const {setStatus} = useStep()
     const [response] = useResponse(hRef)
     const statusReport = response && getStatusReport(response && response.data) || []
     const generateMessageLines = (lines: any) => lines.map((line: any) => ({
@@ -17,7 +17,7 @@ const StatusReportPreview = ({hRef}: any) => {
     const handleOnClick = (item:any, message:any) => {
         const property = item.id
         console.log('item', item, message)
-        setFocusError({hRef, property, message })
+        setStatus({hRef, property, status:'error', message })
     }
 
     const Sections = statusReport && statusReport.messages && statusReport.messages.map((message: any, index: any) => <PanelSection key={index} title={message.message}
--- src/hooks/useStep.tsx
+++ src/hooks/useStep.tsx
@@ -1,22 +1,85 @@
 import * as aiaReducer from 'store/reducers/aiaReducer';
 import {useCallback, useContext} from 'react';
 import baContext from 'context/baContext';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {getStatusReport, isResponseConsistent} from 'utils/functions';
 
 const useStep = () => {
     const dispatch = useDispatch()
     const context = useContext(baContext)
     const baId: any = context.baId
+    const currentStep = useSelector((state: any) => state.aia[baId] && state.aia[baId].steps.current)
+    const stepRessource = useSelector((state: any) => state.aia[baId] &&
+        state.aia[baId].steps &&
+        state.aia[baId].steps[currentStep])
+    const rawResponses = useSelector((state: any) => state.aia[baId] &&
+        state.aia[baId])
 
-    const setFocusError = useCallback ( ({hRef, property, message}:any) => {
-        dispatch(aiaReducer.aiaStepSetInputStatus({baId, hRef, property, status:{value:'error', message}}))
-    },[baId, dispatch])
+    //We take only hRef, not steps and status
+    const responses = rawResponses.filter((ressource: string) => ressource.slice(0, 4) === 'http')
 
-    const setCurentStep = useCallback ( (current:string) => {
+    const setStatus = useCallback(({hRef, property, status, message }: any) => {
+        dispatch(aiaReducer.aiaStepSetInputStatus({baId, hRef, property, status: {value: status, message}}))
+    }, [baId, dispatch])
+
+    const setCurentStep = useCallback((current: string) => {
         dispatch(aiaReducer.aiaStepSetCurrent({baId, current}))
-    },[baId, dispatch])
+    }, [baId, dispatch])
 
-    return {setFocusError,setCurentStep}
+    /**
+     * This function return all messages returned by the API in the status-report related to the current step
+     * based Only on input or bind items displayed
+     * The messages are split into errors and warnings variables
+     * @return {any} Return the message list
+     */
+    const getMessages = () => {
+        const errors: any = []
+        const warnings: any = []
+
+        stepRessource && Object.values(stepRessource)
+            .forEach(([hRef, properties]: any) => Object.entries(properties)
+                .forEach(([property, propertyStatus]: any) => {
+                    const statusValue = propertyStatus.status.value
+                    if (statusValue === 'error')
+                        errors.push({hRef, property, propertyStatus})
+                    if (statusValue === 'warn')
+                        warnings.push({hRef, property, propertyStatus})
+                }))
+
+        return {errors, warnings}
+    }
+
+    /**
+     * We loop on all hRef of the current step. we check is its consistent, if not, we loop on all status_report
+     * messages and se set the status on the bound inputs.
+     * @return {boolean} indicates if it's validated or not
+     */
+    const validate = () => {
+        responses && Object.values(responses)
+            .forEach(([hRef, response]: any) => {
+                if (!isResponseConsistent(response)) {
+                    const statusReport = getStatusReport(response && response.data) || []
+
+                    statusReport.messages.forEach(
+                        (message: any) => {
+                            message.context.forEach(
+                                (line: any) => (
+                                    setStatus({
+                                        hRef, 
+                                        property: line.propertyNames[0],
+                                        status:'error',
+                                        message:message.message})
+                                )
+                            )
+                        }
+                    )
+                }
+            })
+
+        console.log()
+    }
+
+    return {validate, getMessages, setStatus, setCurentStep}
 }
 
 export default useStep;
