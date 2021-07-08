68a0222157b01e00ad970a3439aaa4a1c317cc5b
=Norbert POINTU <=npointu@dxc.com>
2021-07-08 17:30:26 +0200

    use useLabelValue everyWhere

--- src/components/LabelInline/LabelInline.tsx
+++ src/components/LabelInline/LabelInline.tsx
@@ -1,42 +1,6 @@
-import {formatValue, getDescriptionFromOneOf} from 'utils/functions';
-
 import React from 'react';
-import clsx from 'clsx';
 import {makeStyles} from '@material-ui/core/styles';
-import {useTranslation} from 'react-i18next';
-
-export interface LabelInlineProps {
-
-    /**
-     * Property to display
-     */
-    property: any
-
-    /**
-     * Raw data structured as AIA API Rest response
-     */
-    data?: any
-
-    /**
-     * Style of the type + option
-     */
-    styleType?: ['text' | 'currency' | 'percent' | 'decimal' | 'number' | 'date' | 'dateLong', any?]
-
-    /**
-     * isLoading
-     */
-    loading?: boolean
-
-    /**
-     * classname to add to Root classeName
-     */
-    className?: string
-
-    /**
-     * onClick callback
-     */
-    onClick?:any
-}
+import useLabelValue, {useLabelValueProps} from 'hooks/useLabelValue';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -57,30 +21,6 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'left',
         width: '50%',
         overflowWrap: 'break-word',
-
-    },
-    loading: {
-        height: '100%',
-        width: '100%',
-        background: theme.palette.common.white,
-        display: 'flex',
-        alignItems: 'center',
-        lineHeight: '1rem',
-    },
-    loadingItem: {
-        animation: '$glow 1.5s ease-in-out infinite',
-        borderTop: '1px solid #f0f9fb',
-        background: '#eee',
-        height: '1rem',
-        color: 'transparent',
-        cursor: 'progress',
-        display: 'inline-block'
-    },
-    loadingItemLabel: {
-        width: '75%',
-    },
-    loadingItemValue: {
-        width: '75%',
     },
     value: {
         color: theme.palette.text.primary,
@@ -91,36 +31,6 @@ const useStyles = makeStyles((theme) => ({
         width: '50%',
         overflowWrap: 'break-word',
     },
-    text: {
-        textAlign: 'left'
-    },
-    number: {
-        textAlign: 'right'
-    },
-    decimal: {
-        textAlign: 'right'
-    },
-    currency: {
-        textAlign: 'right'
-    },
-    percent: {
-        textAlign: 'right'
-    },
-    date: {
-        textAlign: 'left'
-    },
-    dateLong: {
-        textAlign: 'left'
-    },
-    '@keyframes glow': {
-        '0%': {},
-        '100%': {
-            opacity: 1
-        },
-        '50%': {
-            opacity: 0.5
-        }
-    },
 }));
 
 /**
@@ -128,45 +38,17 @@ const useStyles = makeStyles((theme) => ({
  * @param {props} props Contains information related to the LabelInline
  * @returns {*} Return the LabelInline
  */
-const LabelInline: React.FC<LabelInlineProps> = ({
-    property,
-    data,
-    loading,
-    styleType = ['text'],
-    className,
-    onClick
-}: LabelInlineProps) => {
+const LabelInline: React.FC<useLabelValueProps> = (props: useLabelValueProps) => {
     const classes: any = useStyles();
-    const {t} = useTranslation();
-    const SkeletonLabel = () => <div className={classes.loading}><span
-        className={clsx(classes.loadingItem, classes.loadingItemLabel)}></span></div>
-    const SkeletonValue = () => <div className={classes.loading}><span
-        className={clsx(classes.loadingItem, classes.loadingItemValue)}></span></div>
-
-    /**
-     * Retrieve description for a given data
-     * @returns {*} Return the description
-     */
-    function processDataOutput() {
-        if (data && data[property]) {
-            let value = getDescriptionFromOneOf(data[property], property, data);
-
-            if (styleType)
-                value = formatValue(value, styleType[0], styleType[1])
-
-            return value
-        }
-
-        return ''
-    }
+    const {Label, Value} = useLabelValue(props)
 
     return (
-        <div className={clsx(classes.root, className)} onClick={onClick}>
+        <div className={classes.root}>
             <div className={classes.label}>
-                {loading ? <SkeletonLabel/> : property && t(property)}
+                <Label/>
             </div>
-            <div className={classes.value} onClick={onClick}>
-                {loading ? <SkeletonValue/> : <label dangerouslySetInnerHTML={{__html: processDataOutput()}}/>}
+            <div className={classes.value}>
+                <Value/>
             </div>
         </div>
     );
