911219acc100fe545ae06803c62522265f105b48
=Norbert POINTU <=npointu@dxc.com>
2021-07-08 12:38:04 +0200

    Add step when we mount a component. This step will be used for removing the component

--- src/components/DialogActivityStep/DialogActivityStep.tsx
+++ src/components/DialogActivityStep/DialogActivityStep.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import ActivityStep from 'components/ActivityStep/ActivityStep';
 import useStep from 'hooks/useStep';
 import React, {useEffect, useState} from 'react';
--- src/hooks/useBindInputToStep.tsx
+++ src/hooks/useBindInputToStep.tsx
@@ -10,6 +10,9 @@ const useBindInputToStep = ({hRef, property}: any) => {
     const baId: any = context.baId
     const [inputId] = useState(uniqueId(property))
     const currentStep = useSelector((state: any) => state.aia[baId] && state.aia[baId].steps.current)
+    const [step, setStep] = useState(currentStep) //Important : Save the step when AddInput, it will be used when unmount.
+    // It's prevent the effect of changing current step
+
     const status = useSelector((state: any) => state.aia[baId] &&
         state.aia[baId].steps &&
         state.aia[baId].steps[currentStep] &&
@@ -22,10 +25,11 @@ const useBindInputToStep = ({hRef, property}: any) => {
         state.aia[baId].steps[currentStep][hRef][property].status.message)
 
     useEffect(() => {
-        dispatch(aiaReducer.aiaStepAddInput({baId, hRef, property, inputId}))
+        dispatch(aiaReducer.aiaStepAddInput({baId, hRef, property, step:currentStep, inputId}))
+        setStep(currentStep)
 
         return () => {
-            dispatch(aiaReducer.aiaStepRemoveInput({baId, hRef, property}))
+            dispatch(aiaReducer.aiaStepRemoveInput({baId, hRef, property, step}))
         }
     }, [])
 
