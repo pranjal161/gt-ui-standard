f51a846dcc0b1ff308e1095a261620220c6a3d57
=Norbert POINTU <=npointu@dxc.com>
2021-07-12 08:51:48 +0200

    Put response and loading for useLabelValue

--- src/components/ClientPreview/components/PersonGeneralSection.tsx
+++ src/components/ClientPreview/components/PersonGeneralSection.tsx
@@ -1,16 +1,18 @@
 import PanelSection, {PanelSectionItem} from 'components/PanelSection/PanelSection';
 import ContentList from 'components/ContentList/ContentList';
+import useResponse from 'hooks/useResponse';
 import React from 'react';
 
 const PersonGeneralSection = ({hRef}: any) => {
+    const [response, loading] = useResponse(hRef)
     const items: PanelSectionItem[] = [
-        {hRef, id: 'person:gender', styleType: ['text']},
-        {hRef, id: 'person:first_name', styleType: ['text']},
-        {hRef, id: 'person:last_name', styleType: ['text']},
-        {hRef, id: 'person:birth_date', styleType: ['date']},
-        {hRef, id: 'person:age', styleType: ['number']},
-        {hRef, id: 'person:professional_status', styleType: ['text']},
-        {hRef, id: 'person:language', styleType: ['text']},
+        {hRef, id: 'person:gender', styleType: ['text'], response, loading},
+        {hRef, id: 'person:first_name', styleType: ['text'], response, loading},
+        {hRef, id: 'person:last_name', styleType: ['text'], response, loading},
+        {hRef, id: 'person:birth_date', styleType: ['date'], response, loading},
+        {hRef, id: 'person:age', styleType: ['number'], response, loading},
+        {hRef, id: 'person:professional_status', styleType: ['text'], response, loading},
+        {hRef, id: 'person:language', styleType: ['text'], response, loading},
     ]
 
     return <PanelSection title={'Detail'}
