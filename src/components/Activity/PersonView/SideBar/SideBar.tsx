401a98f245c181753a39ac9715f8b9595357c963
=Norbert POINTU <=npointu@dxc.com>
2021-07-13 12:24:48 +0200

    Add contracts in sidebar for PersonView

--- src/components/Activity/PersonView/SideBar/SideBar.tsx
+++ src/components/Activity/PersonView/SideBar/SideBar.tsx
@@ -1,34 +1,41 @@
 import ContractSideBar from 'components/Activity/ContractOperation/SideBar/ContractSideBar/ContractSideBar';
 import GlobalSideBar from 'components/SideBar/SideBar';
+import {APIConfig} from 'configs/apiConfig';
 import React from 'react';
 import useResponse from 'hooks/useResponse';
 import { useSidebar } from 'hooks/useSidebar';
 import useTabs from 'hooks/useTabs';
 import { useTranslation } from 'react-i18next';
+import {getCollectionItems} from 'utils/functions';
 
 const contractController = (value: any) => <ContractSideBar hRef={value.id}/>
 const loadingController = () => <div>Loading</div>
 
 const SideBar = (props: any) => {
-    const {mainEntityHRef} = props
+    const {hRef} = props
     const {t} = useTranslation()
     const {openNewTab, openNewTabInSecondaryWindow, forContract} = useTabs()
-    const [mainEntityResponse] = useResponse(mainEntityHRef)
+
+    const contractsHRef = APIConfig().defaultHostUrl + 'contracts?_mode=individual_contract&_inquiry=cs_contract_owner_contract_list&party_role:person=' + hRef
+
+    const [contractsResponse] = useResponse(contractsHRef)
 
     let items: any = {}
 
-    const mainEntitySummary = mainEntityResponse && mainEntityResponse.data._links.self
-    if (mainEntitySummary) {
+    const contracts = contractsResponse && getCollectionItems(contractsResponse.data)
 
-        const title = mainEntityResponse.data['contract:number']
-        items.contract = [{
-            title,
-            display: t('common:contractNumberTitle', {value: title}),
-            id: mainEntitySummary.href,
-            hRef: mainEntitySummary.href,
-            entityType: 'contract',
-            controller: contractController,
-        }]
+    if (contracts) {
+        items.contract = contracts.map((contract:any) => {
+            const title = contract.summary['contract:number']
+            
+            return {
+                title,
+                display: t('common:contractNumberTitle', {value: title}),
+                id: contract.href,
+                hRef: contract.href,
+                entityType: 'contract',
+                controller: contractController,} 
+        })
     }
     else
         //This is a workaround for the initial state and to have contract define by default
