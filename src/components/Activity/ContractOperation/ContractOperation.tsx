9fae6d58fd3dba910f7427d789716b4e330a9713
Maxime Mary <maxime.mary@dxc.com>
2021-07-21 11:19:00 +0200

    Remove modification in ContractOperation

--- src/components/Activity/ContractOperation/ContractOperation.tsx
+++ src/components/Activity/ContractOperation/ContractOperation.tsx
@@ -1,6 +1,5 @@
 import React, {useCallback, useState} from 'react';
 import Stepper, {StepProps} from 'components/Stepper/Stepper';
-import { getLink, hasMethodInOptions } from 'utils/functions';
 
 import ActivityStep from 'components/ActivityStep/ActivityStep';
 import Button from 'components/Button/Button';
@@ -72,20 +71,15 @@ const ContractOperation: React.FC<any> = (props: {hRef:string}) => {
 
     const [currentStep, setCurrentStep] = useState(0);
     const [activityResponse] = useResponse(hRef);
-    const [moneyInReponse] = useResponse(activityResponse && getLink(activityResponse.data, 'cscaia:money_in'))
-    const {patch, post} = useAia();
+    const {patch} = useAia();
 
     const SideBarConf = configurations.sidebar
 
     const nextStep = useCallback ((index: number) => {
-        validateStep().then(async(inputErrors:any) => {
+        validateStep().then((inputErrors:any) => {
             if (inputErrors.length === 0) {
                 const step = index >= steps.length ? steps.length - 1 : index;
-                // HERE I check if the current UP has a money in
-                if(moneyInReponse && hasMethodInOptions(moneyInReponse.data, 'PATCH')) {
-                    // HERE I save the money in in API to unlock some parts in the UP
-                    await post(`${activityResponse.data._links['cscaia:money_in'].href}/save`, {})
-                }
+                
                 setCurrentStep(step);
             }
             else {
